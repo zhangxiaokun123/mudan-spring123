97bb0f2ecc3a49a5a341902bd37721b7cfa95fc3
zhangjr317 <417017130@qq.com>
2019-10-15 09:10:13 +0800

    roomcontroller

---
+++ src/main/java/com/cy/controller/RoomController.java
@@ -0,0 +1,9 @@
+package com.cy.controller;
+
+import org.springframework.stereotype.Controller;
+@Controller
+public class RoomController {
+    public String doFindObjects(){
+        return null;
+    }
+}
